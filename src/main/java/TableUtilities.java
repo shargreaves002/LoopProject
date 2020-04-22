c0d78265ca6140dde53df3d61f896277e29a3889
cephc <chatyrac@icloud.com>
2020-04-22 12:31:28 -0400

    finished

--- src/main/java/TableUtilities.java
+++ src/main/java/TableUtilities.java
@@ -2,17 +2,22 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return getMultiplicationTable(10);
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return getMultiplicationTable(2);
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
         String outcome = "";
-        for(int i = 1; i <= tableSize; i++){
-            outcome += i * tableSize + " |" + "\n";
-        } return outcome;
+        for(int i = 1; i <= tableSize; i++) {
+            for(int x= 1; x <= tableSize; x++) {
+                int numOfSpaces = Math.max(0, 2 - (int) (Math.log10(i * x)));
+                outcome += " ".repeat(numOfSpaces) + (i * x) + " |";
+            }
+            outcome += "\n";
+        }
+        return outcome;
     }
 }
